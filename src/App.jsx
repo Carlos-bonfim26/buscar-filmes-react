d41eb079d96bd38a0ea2a8f1fbbf6831946b32ce
carlos <carlosbonfim722@gmail.com>
2024-05-18 20:39:38 -0300

    version1.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import BuscaDeFilmes from './components/BuscaDeFilmes'
+import './App.css'
+
+function App() {
+
+
+  return (
+  <BuscaDeFilmes/>
+  )
+}
+
+export default App
